bbdf09a5906d2c29493180979145518e77ccc83e
Nsude <nsudesolo@gmail.com>
2023-09-17 17:43:20 -0700

    werror task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the code
+ *
+ * Return: should be 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
